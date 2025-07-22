b0476c8dd473583f0b4c26b0d86ed1238af37dff
neerajsinght79-alt <neerajsinght79@gmail.com>
2025-07-22 08:49:32 +0530

    Update shrinkme.py

--- shrinkme.py
+++ shrinkme.py
@@ -1,9 +1,9 @@
 import aiohttp
 from config import SHRINKME_API_KEY
 
-async def create_shortlink(original_url, user_id):
-    api_url = f"https://shrinkme.io/api?api={SHRINKME_API_KEY}&url={original_url}&alias=user{user_id}"
+async def get_shortlink(url):
+    api_url = f"https://shrinkme.io/api?api={SHRINKME_API_KEY}&url={url}"
     async with aiohttp.ClientSession() as session:
         async with session.get(api_url) as response:
             data = await response.json()
-            return data.get("shortenedUrl") or original_url
+            return data.get("shortenedUrl", url)
