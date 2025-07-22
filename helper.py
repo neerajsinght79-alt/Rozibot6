c74d3453f79cbcf54d09d8bde0e689da57a07a6c
neerajsinght79-alt <neerajsinght79@gmail.com>
2025-07-22 08:48:48 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,13 +1,17 @@
-# helper.py
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import UPDATES_CHANNEL
 
 user_steps = {}
 
-def save_user_step(user_id, step):
-    user_steps[user_id] = step
+def save_user_step(user_id, data):
+    user_steps[user_id] = data
 
 def get_user_step(user_id):
-    return user_steps.get(user_id, None)
+    return user_steps.get(user_id)
 
-def clear_user_step(user_id):
-    if user_id in user_steps:
-        del user_steps[user_id]
+def force_join_keyboard():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔔 Join Update Channel", url=f"https://t.me/{UPDATES_CHANNEL.strip('@')}")],
+        [InlineKeyboardButton("✅ Joined", callback_data="verify_join")]
+    ])
